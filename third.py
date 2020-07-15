2ae4c6f5e2d810e9574acb38e97352df30ecd366
sachinskbaaglar <68234630+sachinskbaaglar@users.noreply.github.com>
2020-07-15 11:57:18 +0530

    Add files via upload

---
+++ third.py
@@ -0,0 +1,18 @@
+first_name=input("enter your first_name")
+last_name=input("enter your last_name")
+full_name=first_name+last_name
+print("")
+age=int(input("please enter your age"))
+print("")
+course=input("please enter your course")
+print("")
+contact=int(input("please enter your contact"))
+print("")
+
+print("The name you have entered is",full_name)
+
+print("The name you have entered is",age)
+
+print("The name you have entered is",course)
+
+print("The name you have entered is",contact)
